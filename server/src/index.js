78970040f8499d795adcef1a0cc1ee3279404653
Neha Rajbhar <126654388+Neha-rajbhar@users.noreply.github.com>
2023-08-25 16:06:10 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -11,7 +11,11 @@ const Contact = require("./model/contact.js");
 const login = require("./model/login.js");
 const port = process.env.PORT || 5000;
 app.use(multer().any());
-app.use(cors());
+app.use(cors({
+  origin: [],
+  method:["POST","GET"],
+  credentials:true,
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
